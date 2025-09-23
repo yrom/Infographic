99ee0ae92b5d519c7291695827cb07fd5177a433
Aaron <antvaaron@gmail.com>
2025-09-23 11:39:27 +0800

    feat(dev): support select options and cache selection

--- packages/dev/src/renderer.ts
+++ packages/dev/src/renderer.ts
@@ -11,55 +11,67 @@ registerResourceLoader(async (config) => {
   return loadSVGResource(str);
 });
 
-const container = document.getElementById('root');
+export interface RenderOptions {
+  structure?: string;
+  item?: string;
+  background?: string;
+}
 
-const infographic = new Infographic({
-  container,
-  width: 800,
-  height: 600,
-  padding: 20,
-  design: {
-    structure: { type: 'list-column', gap: 5 },
-    title: 'default',
-    item: { type: 'pyramid', positionH: 'flipped' },
-  },
-  data: {
-    title: 'AntV Infographic',
-    desc: 'AntV Infographic is an AI-powered infographic recommendation and generation tool',
-    items: [
-      {
-        icon: '0-001_v1_lineal',
-        label: 'AntV G',
-        desc: 'Flexible visualization rendering engine',
-      },
-      {
-        icon: '0-013_v1_lineal',
-        label: 'AntV G2',
-        desc: 'Progressive visualization grammar',
-      },
-      {
-        icon: '10-036_v1_lineal',
-        label: 'AntV G6',
-        desc: 'Simple, easy-to-use, and comprehensive graph visualization engine',
-      },
-    ],
-  },
-  themeConfig: {
-    background: 'black',
-    palette: [
-      '#1783FF',
-      '#00C9C9',
-      '#F0884D',
-      '#D580FF',
-      '#7863FF',
-      '#60C42D',
-      '#BD8F24',
-      '#FF80CA',
-      '#2491B3',
-      '#17C76F',
-      '#70CAF8',
-    ],
-  },
-});
+export function render({
+  structure = 'list-column',
+  item = 'pyramid',
+  background,
+}: RenderOptions) {
+  const container = document.getElementById('root');
 
-infographic.render();
+  const infographic = new Infographic({
+    container,
+    width: 800,
+    height: 600,
+    padding: 20,
+    design: {
+      structure: { type: structure, gap: 5 },
+      title: 'default',
+      item: { type: item },
+    },
+    data: {
+      title: 'AntV Infographic',
+      desc: 'AntV Infographic is an AI-powered infographic recommendation and generation tool',
+      items: [
+        {
+          icon: '0-001_v1_lineal',
+          label: 'AntV G',
+          desc: 'Flexible visualization rendering engine',
+        },
+        {
+          icon: '0-013_v1_lineal',
+          label: 'AntV G2',
+          desc: 'Progressive visualization grammar',
+        },
+        {
+          icon: '10-036_v1_lineal',
+          label: 'AntV G6',
+          desc: 'Simple, easy-to-use, and comprehensive graph visualization engine',
+        },
+      ],
+    },
+    themeConfig: {
+      background,
+      palette: [
+        '#1783FF',
+        '#00C9C9',
+        '#F0884D',
+        '#D580FF',
+        '#7863FF',
+        '#60C42D',
+        '#BD8F24',
+        '#FF80CA',
+        '#2491B3',
+        '#17C76F',
+        '#70CAF8',
+      ],
+    },
+  });
+
+  infographic.render();
+}
